2ca66b2f874d9432d099de3c2c8992605036c150
mayani <arti.agni01@gmail.com>
2022-07-09 14:53:26 +0530

    db bug fix

--- back-end/database/db.js
+++ back-end/database/db.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password, dbName) => {
-    const URL = `mongodb+srv://${username}:${password}@cluster0.froa2.mongodb.net/?retryWrites=true&w=majority`;
+    const URL = `mongodb+srv://${username}:${password}@cluster0.froa2.mongodb.net/${dbName}?retryWrites=true&w=majority`;
     try {
-        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true});
+        await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
         console.log("You are connected successfully!");
     } catch (error) {
         console.log("Cannot connect to MongoDb. Error: " + error);
