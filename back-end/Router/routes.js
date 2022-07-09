8abcf31c0cfb9c985c539a0c638852264653bb1e
mayani <arti.agni01@gmail.com>
2022-07-09 12:39:15 +0530

    express bug fix

--- back-end/Router/routes.js
+++ back-end/Router/routes.js
@@ -1,4 +1,9 @@
-import express from "express";
+import Express from "express";
 import { storeContactConsent } from "../controller/consent_controller";
 
+
+const route = Express.Router();
+
 route.post('/storeContactConsent', storeContactConsent);
+
+export default route;
